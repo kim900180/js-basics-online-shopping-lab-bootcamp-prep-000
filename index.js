1688e4455867c62ef23d9ea9a1c58680b675832d
Jae Yong Kim <kim900180@gmail.com>
2018-07-11 17:29:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  for (var = 0; i < cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
     }
